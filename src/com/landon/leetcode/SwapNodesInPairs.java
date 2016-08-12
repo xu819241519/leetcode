ed5e25df923d51869a86b70b894b69138568e785
landon <819241519@qq.com>
2016-08-12 22:21:15 +0800

    添加第24题

--- src/com/landon/leetcode/SwapNodesInPairs.java
+++ src/com/landon/leetcode/SwapNodesInPairs.java
@@ -1,5 +1,60 @@
 package com.landon.leetcode;
 
+import java.util.List;
+
 public class SwapNodesInPairs {
 
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	public ListNode swapPairs(ListNode head) {
+		ListNode result = head;
+		ListNode p = head, q;
+		if (head == null || head.next == null) {
+			return head;
+		} else {
+			head = q = p.next;
+			boolean isFirstPair = true;
+			while (q != null) {
+				if (isFirstPair) {
+					p.next = q.next;
+					q.next = p;
+					q = p.next;
+				}else{
+					if(q.next != null){
+						p.next = q.next;
+						p = p.next;
+						q.next = p.next;
+						p.next = q;
+						q = q.next;
+						p = p.next;
+					}else{
+						break;
+					}				
+				}
+				isFirstPair = false;
+			}
+			return head;
+		}
+	}
+
+	public void test() {
+		ListNode head = new ListNode(0);
+		ListNode p = head;
+		for (int i = 0; i < 10; ++i) {
+			p.next = new ListNode(i + 1);
+			p = p.next;
+		}
+		ListNode result = swapPairs(head);
+		while (result != null) {
+			System.out.println(result.val);
+			result = result.next;
+		}
+	}
 }
