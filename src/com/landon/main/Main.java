58e920b71e89877042b1434ba4183ee9f6ffe20b
landon <819241519@qq.com>
2017-09-24 21:17:55 +0800

    添加code

--- src/com/landon/main/Main.java
+++ src/com/landon/main/Main.java
@@ -1,7 +1,12 @@
 package com.landon.main;
 
+import java.util.List;
+
+import com.landon.leetcode.DivideTwoIntegers_29;
 import com.landon.leetcode.LongestPalindromicSubString;
 import com.landon.leetcode.Median2SortedArrays;
+import com.landon.leetcode.StrStr;
+import com.landon.leetcode.SubstringWithConcatenation_30;
 import com.landon.leetcode.ZigZagConversion;
 
 public class Main {
@@ -10,13 +15,29 @@ public class Main {
 //		Median2SortedArrays median2SortedArrays = new Median2SortedArrays();
 //		int [] nums1 = {1,3};
 //		int [] nums2 = {2};
-//		System.out.println("result£º" + median2SortedArrays.findMedianSortedArrays(nums1,nums2));
+//		System.out.println("resultï¿½ï¿½" + median2SortedArrays.findMedianSortedArrays(nums1,nums2));
 
 //		LongestPalindromicSubString subString = new LongestPalindromicSubString();
 //		System.out.println(subString.longestPalindrome("b"));
 		
-		ZigZagConversion conversion = new ZigZagConversion();
-		System.out.println(conversion.convert("PAYPALISHIRING", 4));
+//		ZigZagConversion conversion = new ZigZagConversion();
+//		System.out.println(conversion.convert("PAYPALISHIRING", 4));
+//		
+//		StrStr str = new StrStr();
+//		System.out.println(str.strStr("abc", "bc"));
+		
+//		DivideTwoIntegers_29 divide = new DivideTwoIntegers_29();
+//		System.out.println(divide.divide(-2147483648, 1));
+		
+		
+		
+	
+		
+		SubstringWithConcatenation_30 concate = new SubstringWithConcatenation_30();
+		List<Integer> result = concate.findSubstring("aoucbmnvdujheznbntaszqsxicczlagnqbrsnfptbycfapjnkjflbzilemkpotehwvblcsefqgnsxwgkhnvjpgutuhklcosylvjonqtfyiyyegimtfxilrdiantcdncpiofxgaegjcenkobguyqhsupsjkxnxbehrjgxjlespiiazjiviyeaswuegtrexxnogumrfskwcuwbfgynfdpzzmdfhwletbvbvjtcbfydbxhgdfuiilkhznpjmcnhdkjytecujbykafqdkmovaacbntkbwyjziuaeycyhfytfdllqybnabpbqlmujmdiwxkaxnzeuxzcdknvkqimtzojkcdtoiemhonedokanjcswpnihvvaxlljprdfzjrhzgnfwyfkjhchyssfppfmaqwrjbwjmnslwhqsfverejacbshshohjhdaqgwzsmtfkbycitjzasccvukpcywlhboyjkzdyvjiwhngcwicqkhnercgrzuzcizmuyptvadrhqymmgqeqxrwqwfivzjzjklfkbygbczlszzhfpnxpfwfdbpacazlklqxordrveepedwlvmjktfrwihwkjvvugntweyqzcupgynstzdfskwqsfmcpixlqmenrcfsezjlxzdsyiztswjkdymsgldwxhlqlqjqsudptikuqjpihyslwgderjdqsqhejswbmzqihcczorhvrbiqhouaxxqroxvxrragssozvqajhhgakrqrfltekkvajtzbkzhfsvepnfawoiwzznsgammmykphdoipqrukobzbizxyhuxjjsrjgexgomntbyktphdekchsdfqmbqxkkpvstsyjfleilqbdxgjhkqbnvsbwkzguodmjwkubxaljqomouvxelztjtwhdvqzltlwpxssusffjtrznowtavmlojstgisuefsvclozdteopwnckmwmjkzavstecoyitsewduvjpzzexnjkbhykrympsitwwtfpllnrfiaukzzjoivrrueisqxmysiulpmzazqfkqcwrbfilbzcxfmrwmdwelrsbfrdehjqznmsquabxcfuhtlfhqcmbvgeaxkggvxfilxyfabecgalbnrjdtxhodnqcxwisvpahmyomztqhveljvumotteyhuagskzozbxlclabgslcwylruzhnvnlejnkcxlswnpjrajsjefnadauxzbmwrzaamnclauhplrgocbxggkjmkdllgykzzkamzcxazhpkywycxxlfhuttzfhhfrhedjqfnqfmxwzxuxztxmzgischzjrecajhjbmwrtlqqknmjpgg", new String[]{"mntbyktphdekchsdfqmbq","zznsgammmykphdoipqruk","hyssfppfmaqwrjbwjmnsl","hkqbnvsbwkzguodmjwkub","qsqhejswbmzqihcczorhv","xaljqomouvxelztjtwhdv","dptikuqjpihyslwgderjd","sozvqajhhgakrqrfltekk","lszzhfpnxpfwfdbpacazl","tavmlojstgisuefsvcloz","wjkdymsgldwxhlqlqjqsu","dteopwnckmwmjkzavstec","obzbizxyhuxjjsrjgexgo","rwqwfivzjzjklfkbygbcz","whngcwicqkhnercgrzuzc","frwihwkjvvugntweyqzcu","rbiqhouaxxqroxvxrrags","qmenrcfsezjlxzdsyizts","cvukpcywlhboyjkzdyvji","xkkpvstsyjfleilqbdxgj","ykrympsitwwtfpllnrfia","daqgwzsmtfkbycitjzasc","whqsfverejacbshshohjh","oyitsewduvjpzzexnjkbh","izmuyptvadrhqymmgqeqx","klqxordrveepedwlvmjkt","qzltlwpxssusffjtrznow","pgynstzdfskwqsfmcpixl","vajtzbkzhfsvepnfawoiw"});
+		for(int i = 0; i < result.size(); ++i) {
+			System.out.println(result.get(i));
+		}
 	}
 
 }
